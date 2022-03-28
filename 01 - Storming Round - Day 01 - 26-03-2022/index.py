71436dba699741854a57bb2dd1d9d94259f6fe92
Chamaru Amasara <mailtochamaru@gmail.com>
2022-03-28 23:27:05 +0530

    all the updates

--- 01 - Storming Round - Day 01 - 26-03-2022/index.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# Parse the time strings
-base=datetime(2021,10,1)
-timeSinceBase=datetime(2021,10,4)-base
-
-print(type(timeSinceBase))
-reduce=timeSinceBase//7//4
-print(datetime("2021-10-04"),timeSinceBase,reduce)
